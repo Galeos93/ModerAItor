e2c7c7801668c32aaa83fe25c7900e5ad5cbc135
Alejandro Garcia <alejandro.garcia.ihs@gmail.com>
2023-06-18 20:01:26 +0200

    Create private HF endpoint. Minor changes on DEMO

--- moderaitor/llm/openassistant.py
+++ moderaitor/llm/openassistant.py
@@ -1,6 +1,7 @@
 import os
 
 from langchain import HuggingFaceHub, PromptTemplate
+from langchain.llms.huggingface_endpoint import HuggingFaceEndpoint
 from langchain.chains import LLMChain
 
 from moderaitor.models import ACCEPTED_ITEM, SUSPICIOUS_ITEM
@@ -14,6 +15,7 @@ def _get_api_key():
 os.environ['HUGGINGFACEHUB_API_TOKEN'] = _get_api_key()
 
 REPO_ID = "OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5"
+ENDPOINT_URL = "https://v33umrijrnokebdq.us-east-1.aws.endpoints.huggingface.cloud"
 
 template = f"""Write '{ACCEPTED_ITEM}' if post follows the rules, '{SUSPICIOUS_ITEM}' otherwise.
 The rules are: {{rules}}.
@@ -24,8 +26,14 @@ Result:
 
 def get_model() -> LLMChain:
     prompt = PromptTemplate.from_template(template)
-    llm = HuggingFaceHub(
-        repo_id=REPO_ID,
+    # llm = HuggingFaceHub(
+    #     repo_id=REPO_ID,
+    # )
+    llm = HuggingFaceEndpoint(
+        endpoint_url=ENDPOINT_URL,
+        huggingfacehub_api_token=os.environ['HUGGINGFACEHUB_API_TOKEN'],
+        task='text-generation',
+        model_kwargs={'return_full_text': True}
     )
     llm_chain = LLMChain(llm=llm, prompt=prompt)
     return llm_chain
